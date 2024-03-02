0fb7a3e1fbf9f183a38dc90290c9f109c754cdfb
Mizanti <156510920+mizanti@users.noreply.github.com>
2024-03-01 23:47:37 -0300

    + local storage functionality for entries

--- notefy/app.js
+++ notefy/app.js
@@ -1,21 +1,28 @@
-let storage = [];
+let webItems = [];
+let localItems = JSON.parse(localStorage.getItem("items"));
 
 const inputEl = document.querySelector("#input-el");
 const saveEl = document.querySelector("#save-el");
 const entriesEl = document.querySelector("#entries-el");
 
-saveEl.addEventListener("click", save);
+if (localItems) {
+    webItems = localItems;
+    listEntries();
+}
 
 function save() {
-    storage.push(inputEl.value);
+    webItems.push(inputEl.value);
+    localStorage.setItem("items", JSON.stringify(webItems))
     inputEl.value = "";
     listEntries();
 }
 
 function listEntries() {
     let list = "";
-    for (let i = 0; i < storage.length; i++) {
-        list += `<li><a href="${storage[i]}" target="_blank">${storage[i]}</a></li>`  
+    for (let i = 0; i < webItems.length; i++) {
+        list += `<li><a href="${webItems[i]}" target="_blank">${webItems[i]}</a></li>`  
     }
     entriesEl.innerHTML = list;
 }
+
+saveEl.addEventListener("click", save);
