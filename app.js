1238a3b86bdaf333be3bcfd5a1306ef311662624
Mizanti <156510920+mizanti@users.noreply.github.com>
2024-03-03 09:02:38 -0300

    + favicon && webmanifest

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
-// GLOBAL VARIABLES
 
 const appSettings = {
     databaseURL: "https://addtocart-a2ae5-default-rtdb.firebaseio.com"
@@ -14,21 +13,24 @@ const inputEl = document.querySelector("#input-el");
 const addBtn = document.querySelector("#add-btn");
 const contentEl = document.querySelector("#content");
 
-//FUNCTIONS
 
 addBtn.addEventListener("click", addCart);
 
 onValue(shoppingDb, function (snapshot) {
-    let itemsArr = Object.entries(snapshot.val());
     contentEl.innerHTML = "";
-    for (const item of itemsArr) {
-        renderOut(item);
+    if (snapshot.exists()) {
+        let itemsArr = Object.entries(snapshot.val());
+        for (const item of itemsArr) {
+            renderOut(item);
+        }        
     }
 });
 
 function addCart() {
-    push(shoppingDb, inputEl.value);
-    inputEl.value = "";
+    if(inputEl.value != "") {
+        push(shoppingDb, inputEl.value);
+        inputEl.value = "";
+    }
 };
 
 function renderOut(value) {
