a87438ee3db2f9890b157c0da8c35047cfd3ecf3
Mizanti <156510920+mizanti@users.noreply.github.com>
2024-03-22 18:05:56 -0300

    working calculator for addition

--- calculator/index.js
+++ calculator/index.js
@@ -1,7 +1,57 @@
-let a = 0;
-let b = 0;
-let operator = '+';
-let display = '';
+let a, b, operator, display;
+
+const expressionEl = document.querySelector('#expression');
+const displayEl = document.querySelector('#display');
+const numbersBtn = document.querySelectorAll('.num');
+const operatorsBtn = document.querySelectorAll('.op');
+const clearBtn = document.querySelector('#clear')
+
+numbersBtn.forEach(number => number.addEventListener('click', updateDisplay));
+operatorsBtn.forEach(operator => operator.addEventListener('click', getValues))
+clearBtn.addEventListener('click', clear);
+
+
+function updateDisplay() {
+    displayEl.value += this.textContent;
+    display = displayEl.value;
+}
+
+function getValues() {
+    const pressedButton = this.textContent;
+
+    if (!a) {
+        a = display
+        operator = pressedButton
+        expressionEl.value = `${a} ${operator} `;
+        displayEl.value = ''
+    } else {
+        b = display;
+        expressionEl.value += `${b} = `;
+        displayEl.value = ''
+    }
+
+    if (pressedButton === '=') operate();
+}
+
+function operate() {
+    operator === "+" ? add() :
+    operator === "-" ? subtract() :
+    operator === "*" ? multiply() :
+    operator === "/" ? divide() :
+    console.log('ERROR');
+}
+
+function add() {
+    displayEl.value = (+a) + (+b);
+}
+
+function clear() {
+    expressionEl.value = '';
+    displayEl.value = '';
+    a = 0;
+    b = 0;
+    operator = null;
+}
 
 //TODO: event listener on '/*-+ keys' to store operator
 //TODO: event listener on '=/*-+ keys' to store values
@@ -18,5 +68,4 @@ let display = '';
     // store the result
     // update display with the solution
     // reset variables
-//TODO: updateDisplay()
 
