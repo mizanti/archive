c23b299a9f49f0734b70dd9e7d5ba0807660b1f9
Mizanti <156510920+mizanti@users.noreply.github.com>
2024-03-25 01:38:13 -0300

    + cumulative calculation & improved style

--- calculator/index.js
+++ calculator/index.js
@@ -1,70 +1,73 @@
-let a, b, operator, display;
+let a, b, operator, solution;
 
 const expressionEl = document.querySelector('#expression');
-const displayEl = document.querySelector('#display');
+const solutionEl = document.querySelector('#solution');
 const numbersBtn = document.querySelectorAll('.num');
 const operatorsBtn = document.querySelectorAll('.op');
-const clearBtn = document.querySelector('#clear')
+const clearBtn = document.querySelector('#clear');
+const clearEBtn = document.querySelector('#clear-e');
+const equalsBtn = document.querySelector('#equals');
 
 numbersBtn.forEach(number => number.addEventListener('click', updateDisplay));
 operatorsBtn.forEach(operator => operator.addEventListener('click', getValues))
+equalsBtn.addEventListener('click', operate);
 clearBtn.addEventListener('click', clear);
+clearEBtn.addEventListener('click', clearE);
 
 
 function updateDisplay() {
-    displayEl.value += this.textContent;
-    display = displayEl.value;
+    if (solutionEl.value === String(solution)) {
+        solutionEl.value = ''
+    }
+
+    solutionEl.value += this.textContent;
 }
 
 function getValues() {
-    const pressedButton = this.textContent;
-
+    
     if (!a) {
-        a = display
-        operator = pressedButton
-        expressionEl.value = `${a} ${operator} `;
-        displayEl.value = ''
+        a = +solutionEl.value;
+        expressionEl.value += `${a} `;
     } else {
-        b = display;
-        expressionEl.value += `${b} = `;
-        displayEl.value = ''
-    }
+        b = +solutionEl.value;
+        expressionEl.value += `${b} `;
+    }    
 
-    if (pressedButton === '=') operate();
+    clear();
+    operate();
+    
+    if (!(this.textContent === '=')) {
+        operator = this.textContent;
+        expressionEl.value += `${operator} `        
+    }    
 }
 
 function operate() {
-    operator === "+" ? add() :
-    operator === "-" ? subtract() :
-    operator === "*" ? multiply() :
-    operator === "/" ? divide() :
-    console.log('ERROR');
-}
+    console.log(a, operator, b)
+    if (a && b && operator) {
+        operator === "+" ? solution = a + b :
+        operator === "-" ? solution = a - b :
+        operator === "*" ? solution = a * b :
+        operator === "/" ? solution = a / b :
+        console.log('ERROR');
+        
+        solutionEl.value = solution;
+        a = solution;
+        b = null;
+        operator = null;
+    }
 
-function add() {
-    displayEl.value = (+a) + (+b);
-}
-
-function subtract() {
-    displayEl.value = (+a) - (+b);
-}
-
-function multiply() {
-    displayEl.value = (+a) * (+b);
-}
-
-function divide() {
-    displayEl.value = (+a) / (+b);
 }
 
 function clear() {
-    expressionEl.value = '';
-    displayEl.value = '';
-    a = 0;
-    b = 0;
-    operator = null;
+    solutionEl.value = ''
 }
 
-//TODO: event listener on '/*-+ keys' to store operator
-//TODO: event listener on '=/*-+ keys' to store values
-//TODO: event listener on '= key' to run operate()
+function clearE() {
+    a = null;
+    b = null;
+    operator = null;
+    solution = null;
+    expressionEl.value = '';
+    solutionEl.value = '';
+}
